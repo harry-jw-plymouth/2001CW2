498f7a0af97e344ee88b64394834f5d43c805163
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2024-12-18 21:12:26 +0000

    Connected trails to db

--- 2001CW/models.py
+++ 2001CW/models.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 import pytz
 
-from config import db
+from config import db, ma
 
 class Trail(db.Model):
     __tablename__ = "TrailTest"
@@ -14,11 +14,20 @@ class Trail(db.Model):
     Difficulty=db.Column(db.String(10))
     Length=db.Column(db.Float)
     Elevation_gain=db.Column(db.Float)
-    Route_type=db.Column(db.String(10)
+    Route_type=db.Column(db.String(10))
     OwnerID=db.Column(db.Integer)
     timestamp = db.Column(
         db.DateTime, default=lambda: datetime.now(pytz.timezone('Europe/London')),
         onupdate=lambda: datetime.now(pytz.timezone('Europe/London'))
     )
+
+class TrailSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Trail
+        load_instance = True
+        sql_session = db.session
+
+trail_schema = TrailSchema()
+trails_schema = TrailSchema(many=True)
     
 
--- 2001CW/trails.py
+++ 2001CW/trails.py
@@ -1,120 +1,52 @@
 # trails.py
 
-from datetime import datetime 
+#from datetime import datetime 
 from flask import abort, make_response
 
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+from config import db
+from models import Trail, trail_schema, trails_schema
 
-TRAILS = {
-    1: {
-        "TrailID": 1,
-        "Trail_name": "Trail 1",
-        "Trail_Summary": "This is summary1",
-        "Trail_Description": "This is description 1",
-        "Difficulty": "Easy", 
-        "Location": "England,England,England",
-        "Length" : 34.4,
-        "Elevation_gain": 23.4,
-        "Route_type": "Loop",
-        "OwnerID": 1,
-        "timestamp": get_timestamp(),
-    },
-    2: {
-        "TrailID": 2,
-        "Trail_name": "Trail 2",
-        "Trail_Summary": "This is summary2",
-        "Trail_Description": "This is description 2",
-        "Difficulty": "Easy", 
-        "Location": "England,England,England",
-        "Length" : 33.2,
-        "Elevation_gain": 3.4,
-        "Route_type": "Loop",
-        "OwnerID": 2,
-        "timestamp": get_timestamp(),
-    },
-    3: {
-        "TrailID": 3,
-        "Trail_name": "Trail 3",
-        "Trail_Summary": "This is summary3",
-        "Trail_Description": "This is description 3",
-        "Difficulty": "Easy", 
-        "Location": "England,England,England",
-        "Length" : 24.3,
-        "Elevation_gain": 12.4,
-        "Route_type": "Loop",
-        "OwnerID": 3,
-        "timestamp": get_timestamp(),
-    }
-    
-}
+
 def read_all():
-    return list(TRAILS.values())
+    trails = Trail.query.all()
+    return trail_schema.dump(trails)
 
 def create(trail):
     TrailID = trail.get("TrailID")
-    Trail_name = trail.get("Trail_name")
-    Trail_summary = trail.get("Trail_Summary")
-    Trail_Description = trail.get("Trail_Description")
-    Difficulty = trail.get("Difficulty")
-    Location = trail.get("Location")
-    Length = trail.get("Length")
-    Elevation_gain = trail.get("Elevation_gain")
-    Route_type = trail.get("Route_type")
-    OwnerID = trail.get("OwnerID")
-    if  TrailID and TrailID not in TRAILS:
-        TRAILS[TrailID] = {
-            "TrailID": TrailID,
-            "Trail_name": Trail_name,
-            "Trail_Summary": Trail_summary,
-            "Trail_Description": Trail_Description,
-            "Difficulty": Difficulty, 
-            "Location": Location,
-            "Length" : Length,
-            "Elevation_gain": Elevation_gain,
-            "Route_type": Route_type,
-            "OwnerID": OwnerID,
-            "timestamp": get_timestamp(),
-        }
-        return TRAILS[TrailID], 201
+    existing_trail = Trail.query.filter(Trail.TrailID == TrailID).one_or_none()
+    if existing_trail is None:
+        new_trail = trail_schema.load(trail, session=db.session)
+        db.session.add(new_trail)
+        db.session.commit()
+        return trail_schema.dump(new_trail), 201
     else:
-        abort(
-            406,
-            f"Trail with id {TrailID} already exists",
-        )
+        abort(406, f"Trail with ID {TrailID} already exists")
 
 def read_one(TrailID):
-    if TrailID in TRAILS:
-        return TRAILS[TrailID]
+    trail = Trail.query.filter(Trail.TrailID == TrailID).one_or_none()
+    if trail is not None:
+        return trail_schema.dump(trail)
     else:
-        abort(
-            404, f"Trail with id {TrailID} not found"
-        )
+        abort(404, f"Trail with id {TrailID} not found")
 
 def update(TrailID, Trail):
-    if TrailID in TRAILS:
-        TRAILS[TrailID]["Trail_name"] = Trail.get("Trail_name", TRAILS[TrailID]["Trail_name"])
-        TRAILS[TrailID]["Trail_Summary"] = Trail.get("Trail_Summary", TRAILS[TrailID]["Trail_Summary"])
-        TRAILS[TrailID]["Trail_Description"] = Trail.get("Trail_Description", TRAILS[TrailID]["Trail_Description"])
-        TRAILS[TrailID]["Difficulty"] = Trail.get("Difficulty", TRAILS[TrailID]["Difficulty"])
-        TRAILS[TrailID]["Location"] = Trail.get("Location", TRAILS[TrailID]["Location"])
-        TRAILS[TrailID]["Length"] = Trail.get("Length", TRAILS[TrailID]["Length"])
-        TRAILS[TrailID]["Elevation_gain"] = Trail.get("Elevation_gain", TRAILS[TrailID]["Elevation_gain"])
-        TRAILS[TrailID]["Route_type"] = Trail.get("Route_type", TRAILS[TrailID]["Route_type"])
-        TRAILS[TrailID]["OwnerID"] = Trail.get("OwnerID", TRAILS[TrailID]["OwnerID"])
-        TRAILS[TrailID]["timestamp"] = get_timestamp()
-        return TRAILS[TrailID]
+    existing_trail = Trail.query.filter(Trail.TrailID == TrailID).one_or_none() 
+    if existing_trail:
+        update_trail = trail_schema.load(trail, session=db.session)
+        existing_trail.TrailID = update_trail.TrailID
+        db.session.merge(existing_trail), 201
     else:
         abort(
             404, f"Trail with ID {TrailID} not found"
         )
 
 def delete(TrailID):
-    if TrailID in TRAILS:
-        del TRAILS[TrailID]
-        return make_response(
-            f"{TrailID} Successfully deleted", 200
-        )
+    existing_trail = Trail.query.filter(Trail.TrailID == TrailID).one_or_none()
+    
+    if exisiting_trail:
+        db.session.delete(existing_trail)
+        db.session.commit()
+        return make_response(f"{TrailID} successfully deleted", 200)
     else:
         abort(
             404, f"Trail with id {TrailID} not found"
